a0e3f2ec8f8e8de25f6f7c256364a8e91c4beab2
Riyadth Al-Kazily <riyadth@gmail.com>
2018-10-21 11:43:09 -0700

    Step 3: Blink an LED

--- IntroToArduino.ino
+++ IntroToArduino.ino
@@ -10,9 +10,25 @@
 void setup()
 {
     // Code placed here will run one time when the Arduino is started
+
+    // Configure the Arduino to make a pin output a voltage
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop()
 {
     // Code placed here will run over and over forever
+
+    // Turn on the LED connected to the pin by providing voltage
+    digitalWrite(LED_BUILTIN, HIGH);
+
+    // Wait for a short time (1000 milliseconds, or 1 second)
+    delay(1000);
+
+    // Turn off the LED by turning off the voltage
+    digitalWrite(LED_BUILTIN, LOW);
+
+    // Wait for a short time (1000 milliseconds, or 1 second)
+    delay(1000);
+
 }
