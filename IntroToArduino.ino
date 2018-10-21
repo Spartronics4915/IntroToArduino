5b8a4dc209be11a5429a701842b78dae68ef1ee1
Riyadth Al-Kazily <riyadth@gmail.com>
2018-10-21 11:30:13 -0700

    Step 2: Comments

--- IntroToArduino.ino
+++ IntroToArduino.ino
@@ -1,8 +1,18 @@
+/*
+  Description: A program to show how code works on the Arduino
+
+  Written by: Riyadth Al-Kazily
+
+  This program is written in several steps, to show how a program
+  will evlolve and change over time.
+*/
 
 void setup()
 {
+    // Code placed here will run one time when the Arduino is started
 }
 
 void loop()
 {
+    // Code placed here will run over and over forever
 }
