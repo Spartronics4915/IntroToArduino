e33b1ed4f43245c49680666906444a301f76adde
Riyadth Al-Kazily <riyadth@gmail.com>
2018-10-21 11:59:11 -0700

    Step 5: Add some NeoPixels

--- IntroToArduino.ino
+++ IntroToArduino.ino
@@ -7,6 +7,18 @@
   will evlolve and change over time.
 */
 
+// Include a library to communicate with colorful LEDs
+#include <Adafruit_NeoPixel.h>
+
+// Name some constant values to make them easy to remember
+// Note that this is a different way than declaring an integer (below)
+#define NEOPIXEL_PIN            6
+#define NUMBER_OF_PIXELS        8
+
+// Tell the computer about the arrangement and type of the pixels we are using
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMBER_OF_PIXELS, NEOPIXEL_PIN,
+                                             NEO_GRB + NEO_KHZ800);
+
 int sensorPin = A0;     // A pin connected to a potentiomenter
 int sensorValue;        // A place to store the value from the potentiometer
 
@@ -16,6 +28,9 @@ void setup()
 
     // Configure the Arduino to make a pin output a voltage
     pinMode(LED_BUILTIN, OUTPUT);
+
+    // Initialize the pixels to get them ready for use
+    pixels.begin();
 }
 
 void loop()
@@ -28,11 +43,19 @@ void loop()
     // Turn on the LED connected to the pin by providing voltage
     digitalWrite(LED_BUILTIN, HIGH);
 
+    // Turn on one NeoPixel
+    pixels.setPixelColor(0, pixels.Color(0, 150, 0));
+    pixels.show();
+
     // Wait for a short time based on the sensor value
     delay(sensorValue);
 
     // Turn off the LED by turning off the voltage
     digitalWrite(LED_BUILTIN, LOW);
+
+    // Turn off the NeoPixel
+    pixels.setPixelColor(0, pixels.Color(0, 0, 0));
+    pixels.show();
 
     // Wait for a short time based on the sensor value
     delay(sensorValue);
